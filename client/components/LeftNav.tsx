cef40ff2ae61de8899d37623f4f4f6711b3b20c7
sairaj <sairaj2119@gmail.com>
2021-07-02 09:23:06 +0530

    fix: changed the brand color

--- client/components/LeftNav.tsx
+++ client/components/LeftNav.tsx
@@ -17,7 +17,7 @@ const LeftNav = () => {
           if (session) push('/home')
           else push('/')
         }}
-        className="flex items-center px-8 my-6 text-2xl font-bold text-purple-600 cursor-pointer group"
+        className="flex items-center px-8 my-6 text-2xl font-bold text-indigo-700 cursor-pointer group"
       >
         <div className="w-10 h-10 transition transform group-hover:rotate-90">
           <svg fill="none" viewBox="0 0 35 32" xmlns="http://www.w3.org/2000/svg">
@@ -41,7 +41,7 @@ const LeftNav = () => {
           onClick={() => push('/home')}
           className="flex items-center px-8 py-4 font-semibold text-gray-600 transition cursor-pointer hover:shadow-md hover:text-gray-800 group"
         >
-          <Home className="w-5 h-5 mr-2 text-lg text-gray-600 fas fa-home group-hover:text-purple-500" />
+          <Home className="w-5 h-5 mr-2 text-lg text-gray-600 fas fa-home group-hover:text-indigo-500" />
           Home
         </li>
         <li className="flex items-center px-8 py-4 font-semibold text-gray-600 transition cursor-pointer hover:shadow-md hover:text-gray-800 group">
