16faa6d013be76f4175d33f4c651029674ce2a71
sairaj <sairaj2119@gmail.com>
2021-07-04 12:57:55 +0530

    refactor(add nav brand icon): add a logo and company name on left navbar

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,5 +1,6 @@
+import '../styles/globals.css'
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
