ae7dadfe030a4b9e4ec31b836a8b751e1e59c7d2
sairaj <sairaj2119@gmail.com>
2021-07-06 21:29:43 +0530

    refactor(edit stories): lessen the padding of circles

--- client/components/organisms/Stories.tsx
+++ client/components/organisms/Stories.tsx
@@ -2,46 +2,66 @@
 
 const Stories = () => {
   return (
-    <div className="px-3 py-4 mt-6 border rounded-lg">
+    <div className="px-3 py-3 mt-6 border rounded-lg">
       <ul className="flex space-x-6">
         <li className="flex flex-col items-center space-y-1 ">
-          <div className="relative p-1 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
-            <a href="#" className="block p-1 bg-white rounded-full">
+          <div className="relative p-0.5 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
+            <a href="#" className="block p-0.5 bg-white rounded-full">
               <img
                 className="w-16 h-16 rounded-full "
                 src="https://placekitten.com/200/200"
                 alt=""
               />
-              <button className="absolute bottom-0 right-0 flex items-center justify-center w-8 h-8 font-mono text-2xl font-semibold text-white bg-blue-500 border-4 border-white rounded-full">
+              <button className="absolute bottom-0 right-0 flex items-center justify-center w-6 h-6 font-mono text-xl font-semibold text-white bg-blue-500 border-2 border-white rounded-full">
                 +
               </button>
             </a>
           </div>
-          <a href="#">Kitty 1</a>
+          <a href="#" className="text-sm font-normal text-gray-600">
+            mewoo
+          </a>
         </li>
         <li className="flex flex-col items-center space-y-1 ">
-          <div className="p-1 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
-            <a href="#" className="block p-1 bg-white rounded-full">
+          <div className="p-0.5 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
+            <a href="#" className="block p-0.5 bg-white rounded-full">
               <img
                 className="w-16 h-16 rounded-full "
-                src="https://placekitten.com/200/200"
+                src="https://placekitten.com/201/200"
                 alt=""
               />
             </a>
           </div>
-          <a href="#">Kitty 1</a>
+          <a href="#" className="text-sm font-normal text-gray-600">
+            cattie
+          </a>
         </li>
         <li className="flex flex-col items-center space-y-1 ">
-          <div className="p-1 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
-            <a href="#" className="block p-1 bg-white rounded-full">
+          <div className="p-0.5 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
+            <a href="#" className="block p-0.5 bg-white rounded-full">
               <img
                 className="w-16 h-16 rounded-full "
-                src="https://placekitten.com/200/200"
+                src="https://placekitten.com/205/200"
                 alt=""
               />
             </a>
           </div>
-          <a href="#">Kitty 1</a>
+          <a href="#" className="text-sm font-normal text-gray-600">
+            kitty
+          </a>
+        </li>
+        <li className="flex flex-col items-center space-y-1 ">
+          <div className="p-0.5 rounded-full bg-gradient-to-tr from-indigo-400 to-pink-400">
+            <a href="#" className="block p-0.5 bg-white rounded-full">
+              <img
+                className="w-16 h-16 rounded-full "
+                src="https://placekitten.com/203/200"
+                alt=""
+              />
+            </a>
+          </div>
+          <a href="#" className="text-sm font-normal text-gray-600">
+            akhil
+          </a>
         </li>
       </ul>
     </div>
