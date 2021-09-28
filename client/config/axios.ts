018b5c7ddc24067981e2d2a225ce2d16626b19c5
sairaj <sairaj2119@gmail.com>
2021-09-28 11:09:16 +0530

    refactor(prefix env var): from api_end_point to next_public_api_endpoint

--- client/config/axios.ts
+++ client/config/axios.ts
@@ -1,5 +1,5 @@
 import axiosMain from 'axios'
 // create axios with config
 export const axios = axiosMain.create({
-  baseURL: process.env.API_END_POINT ?? 'http://localhost:5000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_END_POINT ?? 'http://localhost:5000/api/v1',
 })
