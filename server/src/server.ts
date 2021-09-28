d697d56afee14b55733be545193b18204d123f1c
sairaj <sairaj2119@gmail.com>
2021-09-28 17:36:13 +0530

    refactor(change origin url): to vercel.app

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'production' ? 'https://s2media.vercel.app/' : 'http://localhost:3000',
+    origin: process.env.NODE_ENV === 'production' ? 'https://vercel.app/' : 'http://localhost:3000',
   })
 )
 
