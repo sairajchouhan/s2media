d65fb3e316290faa2e23721ccfde2c993fcbb06d
Sairaj <57995897+sairaj2119@users.noreply.github.com>
2021-10-20 22:23:25 +0530

    chore: testing vscode.dev

--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -3,6 +3,7 @@ import UnAuthenticatedLayout from '../components/layouts/UnAuthenticatedLayout'
 export default function Landing() {
   return (
     <UnAuthenticatedLayout>
+      {/* test commit from vscode.dev */}
       <div className="flex justify-center">
         <h1 className="mt-10 text-5xl text-indigo-500">
           Landing Page Click on SignUp to Get Started
