3bba62ad1185a960cc19ba400e049412f58823c3
sairaj <sairaj2119@gmail.com>
2021-10-25 11:47:41 +0530

    test(eslint): add eslint setup for tesitng

--- apps/client/__tests__/index.test.tsx
+++ apps/client/__tests__/index.test.tsx
@@ -2,8 +2,8 @@ import { render, screen } from '@testing-library/react'
 import React from 'react'
 import Test from '../pages/test-page'
 
-describe('TEst', () => {
-  it('renders a heading', () => {
+describe('TEst', async () => {
+  it('renders a heading', async () => {
     render(<Test />)
 
     const heading = screen.getByRole('heading', {
