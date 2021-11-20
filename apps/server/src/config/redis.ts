5ebec510111608db201198d35884b50245d7d07a
sairaj <sairaj2119@gmail.com>
2021-11-20 11:23:17 +0530

    add docker file for deployment to server

--- apps/server/src/config/redis.ts
+++ apps/server/src/config/redis.ts
@@ -1,2 +1,7 @@
 import Redis from 'ioredis'
-export const redis = new Redis({ host: process.env.REDIS_HOST })
+
+export const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: parseInt(process.env.REDIS_PORT!),
+  password: process.env.REDIS_PASSWORD,
+})
