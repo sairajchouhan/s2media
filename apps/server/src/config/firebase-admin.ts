7c3009b3c8804c486d0bece06991b0b24a64b55f
sairaj <sairaj2119@gmail.com>
2021-11-22 13:02:10 +0530

    fix some bugs

--- apps/server/src/config/firebase-admin.ts
+++ apps/server/src/config/firebase-admin.ts
@@ -1,9 +1,15 @@
 import admin from 'firebase-admin'
-import serviceAccount from '../../admin.json'
 
 export const initAdmin = () => {
+  let serviceAccount: any
+  if (process.env.NODE_ENV === 'production') {
+    serviceAccount = require('../../admin.json')
+  } else {
+    serviceAccount = require('../../admin-dev.json')
+  }
+
   if (admin.apps.length === 0) {
-    console.log(`Initializing Firebase Admin SDK with service account`)
+    console.log(`Initializing Firebase Admin SDK with "${serviceAccount.project_id}" service account`)
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount as any),
     })
