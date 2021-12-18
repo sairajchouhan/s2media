9e06490027c9cfb49313b96d26b6885817f78c84
sairaj <sairaj2119@gmail.com>
2021-12-18 12:37:29 +0530

    add saved page

--- apps/client/pages/saved/index.tsx
+++ apps/client/pages/saved/index.tsx
@@ -1,16 +1,51 @@
 import Head from 'next/head'
+import { useQuery } from 'react-query'
+import { CircleLoader } from '../../components/atoms/CircleLoader'
 import { PageLayout } from '../../components/molecules/Page/page-layout'
 import { PageNav } from '../../components/molecules/Page/page-nav'
+import { Post } from '../../components/organisms/Post'
+import { axios } from '../../config/axios'
+import { useAuth } from '../../context/authContext'
+import { formQueryString } from '../../utils/helpers'
+import { GET_PROFILE_USER_POSTS } from '../../utils/querykeysAndPaths'
 
 const Saved = () => {
+  const { getIdToken, rqUser } = useAuth()
+
+  const {
+    data: posts,
+    isLoading,
+    isError,
+  } = useQuery(GET_PROFILE_USER_POSTS.queryKey(rqUser.username), async () => {
+    const idToken = await getIdToken()
+    const qsObj: Record<string, any> = { username: rqUser.username, save: true }
+    const qs = formQueryString(qsObj)
+    const { data } = await axios.get(GET_PROFILE_USER_POSTS.path(rqUser.username, qs), {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    })
+    return data.posts
+  })
+
+  if (isError) return <div>Something Went worng, Try again :(</div>
+
   return (
-    <PageLayout>
+    <>
       <Head>
-        <title>Saved / S2Media</title>
+        <title>Home / S2Media</title>
       </Head>
-      <PageNav title="Saved" />
-      <main className="px-2">Saved Posts</main>
-    </PageLayout>
+      <PageLayout>
+        <PageNav title="Saved" />
+        <main>
+          {isLoading ? (
+            <CircleLoader className="pt-10" />
+          ) : posts ? (
+            posts.map((post: any) => <Post key={post.id} post={post} />)
+          ) : null}
+        </main>
+      </PageLayout>
+    </>
   )
 }
 
