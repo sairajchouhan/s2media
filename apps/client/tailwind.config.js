576da3e60ec7fd28bb8a21cbe0412a94282a5a43
sairaj <sairaj2119@gmail.com>
2022-01-14 20:47:41 +0530

    upgrade tailwind to V3

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,20 +1,11 @@
 module.exports = {
-  // mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       animation: {
         'bounce-200d': 'bounce 1s infinite 200ms',
         'bounce-400d': 'bounce 1s infinite 400ms',
       },
-    },
-  },
-  variants: {
-    extend: {
-      borderColor: ['focus-visible'],
-      opacity: ['disabled'],
-      backgroundColor: ['active'],
     },
   },
   plugins: [require('@tailwindcss/forms'), require('tailwindcss-debug-screens')],
