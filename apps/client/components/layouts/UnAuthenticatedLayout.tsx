f8edaa7f3f3576631973da7bfdbb412cf8541197
sairaj <sairaj2119@gmail.com>
2022-03-05 21:02:24 +0530

    update

--- apps/client/components/layouts/UnAuthenticatedLayout.tsx
+++ apps/client/components/layouts/UnAuthenticatedLayout.tsx
@@ -20,6 +20,22 @@ const UnAuthenticatedLayout = ({ children }: { children: React.ReactNode }) => {
       {!rqUser ? (
         <>
           <header>
+            <header className="bg-gradient-to-r from-blue-500 to-pink-500/90 px-4 py-2 text-white text-center">
+              <h1>
+                As much I would love to put this project on, but it&apos;s costing me server
+                charges. I may again resume the project in future Thanks for visiting
+                <br />
+                you can always run the project locally by following the instructions in{' '}
+                <a
+                  href="https://github.com/sairajchouhan/s2media/blob/main/README.md"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="font-medium hover:underline cursor-pointer"
+                >
+                  README{' '}
+                </a>
+              </h1>
+            </header>
             <Nav />
           </header>
           <section className="container w-5/6 mx-auto">{children}</section>
