b9be76fe4c040b03b103da4a7cd151d9c28b79bf
sairaj <sairaj2119@gmail.com>
2022-03-12 15:33:23 +0530

    send the envvars to client

--- apps/client/config/axios.ts
+++ apps/client/config/axios.ts
@@ -1,16 +1,18 @@
 import axiosMain from 'axios'
 
 export const SERVER_BASE_URL =
-  process.env.NODE_ENV === 'production' ? `${process.env.BACKEND_URL}` : 'http://localhost:5000'
+  process.env.NODE_ENV === 'production'
+    ? `${process.env.NEXT_PUBLIC_BACKEND_URL}`
+    : 'http://localhost:5000'
 
 export const SERVER_URL =
   process.env.NODE_ENV === 'production'
-    ? `${process.env.BACKEND_URL}/api/v1`
+    ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1`
     : 'http://localhost:5000/api/v1'
 
 export const SEARCH_URL =
   process.env.NODE_ENV === 'production'
-    ? `${process.env.BACKEND_URL}/back/search`
+    ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/back/search`
     : 'http://localhost:5000/back/search'
 
 export const axios = axiosMain.create({
